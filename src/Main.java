d54ee4b18fb824f94f35e58b87c7db892b588863
Hasan8405 <mirshafiei.951@gmail.com>
2024-04-21 16:21:13 +0330

    Revert "Revert "3st commit""
    This reverts commit 483acd39

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
-//ali
+import java.util.Scanner;
 public class Main {
+
+    Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Hel world!");
 
