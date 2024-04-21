6f85af4b217196878241a4fc2df50b7464c71c0e
Hasan8405 <mirshafiei.951@gmail.com>
2024-04-21 16:22:26 +0330

    Revert "4st commit"
    This reverts commit e9dab6ab

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.Scanner;
 public class Main {
-
-    Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Hel world!");
 
