4ebb9f7196aa31084f786c0559befcc336e5eb99
Hasan8405 <mirshafiei.951@gmail.com>
2024-04-21 16:12:53 +0330

    3st commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
 
-
+    Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Hel world!");
 
