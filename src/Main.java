720b0b4cdfae6956983ce79e1e94c6530e4774c9
Hasan8405 <mirshafiei.951@gmail.com>
2024-04-21 16:44:17 +0330

    Hasan commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hel world!");
+System.out.println("Hasan");
 
+//My name is Hasan
         //ridam to in zendegi
     }
 }
